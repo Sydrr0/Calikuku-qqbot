c175bcbb82bd678fa913cf71646447a3ce12708d
Sydrr0 <129657153+Sydrr0@users.noreply.github.com>
2023-05-19 15:22:33 +0800

    Add files via upload
    更新了一些名称的信息

--- mybot/mini_price_get.py
+++ mybot/mini_price_get.py
@@ -1,119 +1,121 @@
 from bs4 import BeautifulSoup
-import requests,time,datetime
+import requests, time, datetime
 
-#a1印花集
+
+# a1印花集
 def get_data_a1yhj():
     t = time.localtime(time.time())
     localtime = time.asctime(t)
     now_time = str(localtime[10:19])
     today_time = str(datetime.date.today())
-    nowa_time = today_time+now_time
-    response = requests.get(url = 'https://buff.163.com/goods/835780#tab=selling')
+    nowa_time = today_time + now_time
+    response = requests.get(url='https://buff.163.com/goods/835780#tab=selling')
     page_url = response.text
-    with open("./cache/price_buff_hjw.html","w",encoding="utf-8") as op:
+    with open("./cache/price_buff_hjw.html", "w", encoding="utf-8") as op:
         op.write(page_url)
-        op = open("./cache/price_buff_hjw.html","r",encoding="utf-8")
-    data_getten = BeautifulSoup(op,"lxml")
-    mini_price = data_getten.find_all("span",class_="custom-currency")
-    
-    
-    #输出名称
+        op = open("./cache/price_buff_hjw.html", "r", encoding="utf-8")
+    data_getten = BeautifulSoup(op, "lxml")
+    mini_price = data_getten.find_all("span", class_="custom-currency")
+
+    # 输出名称
     first_title = (str(data_getten.find("title").get_text))[45:-26]
-    #获取并输出价格   
+    # 获取并输出价格
     now_price = mini_price[1].get("data-price")
     last = ('%s\n----------------------------\n%s: ¥%s'
-            %(nowa_time,first_title,now_price))
+            % (nowa_time, first_title, now_price))
     return last
 
 
-#黄金网
+# 黄金网
 def get_data_hjw():
     t = time.localtime(time.time())
     localtime = time.asctime(t)
     now_time = str(localtime[10:19])
     today_time = str(datetime.date.today())
-    nowa_time = today_time+now_time
-    response = requests.get(url = 'https://buff.163.com/goods/774695?from=market#tab=selling')
+    nowa_time = today_time + now_time
+    response = requests.get(url='https://buff.163.com/goods/774695?from=market#tab=selling')
     page_url = response.text
-    with open("./cache/price_buff_hjw.html","w",encoding="utf-8") as op:
+    with open("./cache/price_buff_hjw.html", "w", encoding="utf-8") as op:
         op.write(page_url)
-        op = open("./cache/price_buff_hjw.html","r",encoding="utf-8")
-    data_getten = BeautifulSoup(op,"lxml")
-    mini_price = data_getten.find_all("span",class_="custom-currency")
-    
-    #输出名称
-    first_title = (str(data_getten.find("title").get_text))[45:-26] 
-    #获取并输出价格   
+        op = open("./cache/price_buff_hjw.html", "r", encoding="utf-8")
+    data_getten = BeautifulSoup(op, "lxml")
+    mini_price = data_getten.find_all("span", class_="custom-currency")
+
+    # 输出名称
+    first_title = (str(data_getten.find("title").get_text))[45:-26]
+    # 获取并输出价格
     now_price = mini_price[2].get("data-price")
     last = ('%s\n----------------------------\n%s: ¥%s'
-            %(nowa_time,first_title,now_price))
+            % (nowa_time, first_title, now_price))
     return last
 
-#石林
+
+# 石林
 def get_data_sl():
     t = time.localtime(time.time())
     localtime = time.asctime(t)
     now_time = str(localtime[10:19])
     today_time = str(datetime.date.today())
-    nowa_time = today_time+now_time
-    response = requests.get(url = 'https://buff.163.com/goods/835615?from=market#tab=selling')
+    nowa_time = today_time + now_time
+    response = requests.get(url='https://buff.163.com/goods/835615?from=market#tab=selling')
     page_url = response.text
-    with open("./cache/price_buff_sl.html","w",encoding="utf-8") as op:
+    with open("./cache/price_buff_sl.html", "w", encoding="utf-8") as op:
         op.write(page_url)
-        op = open("./cache/price_buff_sl.html","r",encoding="utf-8")
-    data_getten = BeautifulSoup(op,"lxml")
-    mini_price = data_getten.find_all("span",class_="custom-currency")
+        op = open("./cache/price_buff_sl.html", "r", encoding="utf-8")
+    data_getten = BeautifulSoup(op, "lxml")
+    mini_price = data_getten.find_all("span", class_="custom-currency")
 
-    #输出名称
-    first_title = (str(data_getten.find("title").get_text))[45:-26] 
-    #获取并输出价格   
+    # 输出名称
+    first_title = (str(data_getten.find("title").get_text))[45:-26]
+    # 获取并输出价格
     now_price = mini_price[2].get("data-price")
     last = ('%s\n----------------------------\n%s: ¥%s'
-            %(nowa_time,first_title,now_price))
+            % (nowa_time, first_title, now_price))
     return last
 
-#usp印花集
+
+# usp印花集
 def get_data_uspyhj():
     t = time.localtime(time.time())
     localtime = time.asctime(t)
     now_time = str(localtime[10:19])
     today_time = str(datetime.date.today())
-    nowa_time = today_time+now_time
-    response = requests.get(url = 'https://buff.163.com/goods/900565#tab=selling')
+    nowa_time = today_time + now_time
+    response = requests.get(url='https://buff.163.com/goods/900565#tab=selling')
     page_url = response.text
-    with open("./cache/price_buff_sl.html","w",encoding="utf-8") as op:
+    with open("./cache/price_buff_sl.html", "w", encoding="utf-8") as op:
         op.write(page_url)
-        op = open("./cache/price_buff_sl.html","r",encoding="utf-8")
-    data_getten = BeautifulSoup(op,"lxml")
-    mini_price = data_getten.find_all("span",class_="custom-currency")
-    #输出名称
+        op = open("./cache/price_buff_sl.html", "r", encoding="utf-8")
+    data_getten = BeautifulSoup(op, "lxml")
+    mini_price = data_getten.find_all("span", class_="custom-currency")
+    # 输出名称
     first_title = (str(data_getten.find("title").get_text))[45:-26]
-    #获取并输出价格   
+    # 获取并输出价格
     now_price = mini_price[1].get("data-price")
     last = ('%s\n----------------------------\n%s: ¥%s'
-            %(nowa_time,first_title,now_price))
+            % (nowa_time, first_title, now_price))
     return last
+
 
 def get_data_zhanhen():
     t = time.localtime(time.time())
     localtime = time.asctime(t)
     now_time = str(localtime[10:19])
     today_time = str(datetime.date.today())
-    nowa_time = today_time+now_time
-    response = requests.get(url = 'https://buff.163.com/goods/835504?from=market#tab=selling')
+    nowa_time = today_time + now_time
+    response = requests.get(url='https://buff.163.com/goods/835504?from=market#tab=selling')
     page_url = response.text
-    with open("./cache/price_buff_hjw.html","w",encoding="utf-8") as op:
+    with open("./cache/price_buff_hjw.html", "w", encoding="utf-8") as op:
         op.write(page_url)
-        op = open("./cache/price_buff_hjw.html","r",encoding="utf-8")
-    data_getten = BeautifulSoup(op,"lxml")
-    mini_price = data_getten.find_all("span",class_="custom-currency")
-    
-    
-    #输出名称
+        op = open("./cache/price_buff_hjw.html", "r", encoding="utf-8")
+    data_getten = BeautifulSoup(op, "lxml")
+    mini_price = data_getten.find_all("span", class_="custom-currency")
+
+    # 输出名称
     first_title = (str(data_getten.find("title").get_text))[45:-26]
-    
-    #获取并输出价格   
+
+    # 获取并输出价格
     now_price = mini_price[2].get("data-price")
     last = ('%s\n----------------------------\n%s: ¥%s'
-            %(nowa_time,first_title,now_price))
+            % (nowa_time, first_title, now_price))
     return last
--- mybot/missing.py
+++ mybot/missing.py
@@ -1,18 +1,19 @@
 import time
-lista = ['一','二','三','四','五','六','七','八','九']
+
+lista = ['一', '二', '三', '四', '五', '六', '七', '八', '九']
 now_time = time.localtime()
 day_num = int(now_time[7]) - 68
-if day_num<=99:
-    num_ge = day_num%10
-    num_shi = day_num//10
-    hanzi_ge = lista[num_ge-1]
-    hanzi_shi = lista[num_shi-1] + '十'
+if day_num <= 99:
+    num_ge = day_num % 10
+    num_shi = day_num // 10
+    hanzi_ge = lista[num_ge - 1]
+    hanzi_shi = lista[num_shi - 1] + '十'
     result = hanzi_shi + hanzi_ge
 else:
-    num_ge = day_num%10
-    num_shi = (day_num-100)//10
-    num_bai = day_num//100
-    hanzi_ge = lista[num_ge-1]
-    hanzi_shi = lista[num_shi-1] + '十'
-    hanzi_bai = lista[num_bai-1] + '百'
+    num_ge = day_num % 10
+    num_shi = (day_num - 100) // 10
+    num_bai = day_num // 100
+    hanzi_ge = lista[num_ge - 1]
+    hanzi_shi = lista[num_shi - 1] + '十'
+    hanzi_bai = lista[num_bai - 1] + '百'
     result = hanzi_bai + hanzi_shi + hanzi_ge
