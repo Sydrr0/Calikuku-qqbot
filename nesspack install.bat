8f5a4d4b423c86c25a7396f6e68bc43f8e35bc13
Sydrr0 <129657153+Sydrr0@users.noreply.github.com>
2023-05-19 15:15:51 +0800

    Add files via upload

---
+++ nesspack install.bat
@@ -0,0 +1,7 @@
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+python -m pip install --upgrade pip
+pip install requests
+pip install lxml
+pip install pycqbot
+pip install bs4
+cmd
